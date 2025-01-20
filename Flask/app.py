6b317f917ba4c8189ec3d180afd757a2fc65e6f8
asa9874 <pjkpjk1129@gmail.com>
2025-01-20 18:08:09 +0900

    Feat: 쿠키 및 세션 관리 기능 추가

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,make_response,session
 
 # Flask 객체 생성
 app = Flask(__name__)  
@@ -41,7 +41,46 @@ def test():
 
 
 
+### 쿠키
+# 쿠키생성
+@app.route('/set_cookie')
+def set_cookie():
+    resp = make_response('쿠키 설정 완료')
+    resp.set_cookie('my_cookie', 'value123')  # 쿠키 설정
+    return resp
 
+
+# 쿠키 읽기
+@app.route('/get_cookie')
+def get_cookie():
+    cookie_value = request.cookies.get('my_cookie')  # 쿠키 값 가져오기
+    return f'쿠키 값: {cookie_value}'
+
+
+
+
+
+app.secret_key = 'testkey'
+
+# 세션 생성
+@app.route('/set_session')
+def set_session():
+    session['user_id'] = 1  # 세션에 사용자 ID 설정
+    return '세션 설정 완료'
+
+
+# 세션 읽기
+@app.route('/get_session')
+def get_session():
+    user_id = session.get('user_id')  # 세션 값 가져오기
+    return f'세션 값: {user_id}'
+
+
+# 세션 제거
+@app.route('/clear_session')
+def clear_session():
+    session.pop('user_id', None)  # 세션 값 삭제
+    return '세션 제거 완료'
 
 if __name__ == '__main__':
     app.run()  # Flask 앱 실행
